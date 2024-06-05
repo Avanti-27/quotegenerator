c15e02996a3898157731d84f0232f31869a62c96
Avanti <avanti.tatwawadi@gmail.com>
2024-06-06 00:54:32 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quote, setQuote] = useState(null);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch("https://type.fit/api/quotes");
+    const res = await data.json();
+    setQuotes(res);
+    setQuote(res[1]);
+  };
+
+  const generateRandomQuote = (quotes) => {
+    return quotes[Math.floor(Math.random() * quotes.length)];
+  };
+
+  const handleClick = () => {
+    setQuote(generateRandomQuote(quotes));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="">
+      <h1 className="font-bold text-5xl text-center p-4 m-10">
+        Quote Generator
+      </h1>
+      <div className="w-[30%] bg-black text-white mx-auto my-0 p-8 m-8 text-center">
+        <button
+          className="border-white border-2 p-3 m-2 text-xl"
+          onClick={handleClick}
         >
-          Learn React
-        </a>
-      </header>
+          New Quote
+        </button>
+        <p className="text-2xl p-2 m-4">{quote?.text}</p>
+        <p className="text-xl p-2">- {quote?.author}</p>
+      </div>
     </div>
   );
 }
